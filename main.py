6d30502b9e7564424f679aae15932a3eeda0b222
ZMiK0 <kbelz21z@gmail.com>
2024-03-09 05:24:44 +0100

    The code lets you search from explorer and splits the song in 3 parts

--- main.py
+++ main.py
@@ -1,6 +1,63 @@
 from pydub import AudioSegment
-t1 = 0 * 1000 #Works in milliseconds
-t2 = 30 * 1000
-newAudio = AudioSegment.from_wav("CancionGiantsVoe.wav")
-newAudio = newAudio[t1:t2]
-newAudio.export('newSong.wav', format="wav") #Exports to a wav file in the current path.
+import tkinter
+from tkinter import filedialog
+
+print("Welcome to 3 Partes")
+#path = str(input("Lets start choosing the song, please, input your directory: "))
+tkinter.Tk().withdraw()
+path = filedialog.askopenfilename()
+songName = path.replace(".wav","")
+songName = songName [::-1]
+print(songName)
+wrongCharacterFound = False
+i = 0
+while(not wrongCharacterFound and i < len(songName)):
+    if songName[i] == "/":
+        songName = songName.split("/", 1)
+        songName = songName[0]
+        wrongCharacterFound = True
+    else:
+        i+=1
+songName = songName[::-1]
+print(songName)
+
+print("Now choose the first interval IN SECONDS:")
+st1 = int(input("Start: "))
+end1 = int(input("End: "))
+print("Great, let's choose the second interval:")
+st2 = int(input("Start: "))
+end2 = int(input("End: "))
+print("Good, let's choose the third")
+st3 = int(input("Start: "))
+end3 = int(input("End: "))
+print("NICE")
+
+
+st1 = st1 * 1000 #Works in milliseconds
+end1 = end1 * 1000
+st2 = st2 * 1000
+end2 = end2 * 1000
+st3 = st3 * 1000
+end3 = end3 * 1000
+
+firstPart = AudioSegment.from_file(path)
+secondPart = AudioSegment.from_file(path)
+thirdPart = AudioSegment.from_file(path)
+
+firstPart = firstPart[st1:end1]
+secondPart = secondPart[st2:end2]
+thirdPart = thirdPart[st3:end3]
+
+path = filedialog.askdirectory()
+print(path)
+
+'''
+firstPart.export(path.replace(".wav", "")  + "1.wav", format="wav") # Exporta el audio
+secondPart.export(path.replace(".wav", "") + "2.wav", format="wav")
+thirdPart.export(path.replace(".wav", "") + "3.wav", format="wav")
+'''
+
+
+firstPart.export(path + "/" + songName + "1Part.wav", format="wav") # Exporta el audio
+secondPart.export(path + "/" + songName + "2Part.wav", format="wav")
+thirdPart.export(path + "/" + songName + "3Part.wav", format="wav")
