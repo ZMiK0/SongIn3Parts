503a38ae72472540e8fac3af0b82ce2b257dce4a
ZMiK0 <kbelz21z@gmail.com>
2024-03-09 17:06:16 +0100

    Now you can choose the ammount of parts do you want to cut

--- main.py
+++ main.py
@@ -3,55 +3,57 @@ import tkinter
 from tkinter import filedialog
 
 print("Welcome to 3 Partes")
-#path = str(input("Lets start choosing the song, please, input your directory: "))
 tkinter.Tk().withdraw()
 path = filedialog.askopenfilename()
-songName = path.replace(".wav","")
-songName = songName [::-1]
-print(songName)
+
+forma = int(input("What type of file do you want to use: \n1. mp3 \n2. wav\nAnswer: "))
+
+goodChoice = False
+while not goodChoice:
+    if forma == 1:
+        songName = path.replace(".mp3", "")
+        goodChoice = True
+    elif forma == 2:
+        songName = path.replace(".wav", "")
+        goodChoice = True
+    else:
+        print("Invalid file type")
+
+songName = songName[::-1]
 wrongCharacterFound = False
 i = 0
-while(not wrongCharacterFound and i < len(songName)):
+while (not wrongCharacterFound and i < len(songName)):
     if songName[i] == "/":
         songName = songName.split("/", 1)
         songName = songName[0]
         wrongCharacterFound = True
+    elif songName[i] == "\\":
+        songName = songName.split("\\", 1)
+        songName = songName[0]
+        wrongCharacterFound = True
     else:
-        i+=1
+        i += 1
+
 songName = songName[::-1]
-print(songName)
+print("Selected song: " + songName)
+partAmmount = int(input("Introduce how many parts do you want: "))
 
-print("Now choose the first interval IN SECONDS:")
-st1 = int(input("Start: "))
-end1 = int(input("End: "))
-print("Great, let's choose the second interval:")
-st2 = int(input("Start: "))
-end2 = int(input("End: "))
-print("Good, let's choose the third")
-st3 = int(input("Start: "))
-end3 = int(input("End: "))
-print("NICE")
+partList = []
 
+for i in range(partAmmount):
+    print(f"Now choose the {i + 1}º interval in seconds:")
+    st = int(input("Start: "))
+    end = int(input("End: "))
 
-st1 = st1 * 1000 #Works in milliseconds
-end1 = end1 * 1000
-st2 = st2 * 1000
-end2 = end2 * 1000
-st3 = st3 * 1000
-end3 = end3 * 1000
-
-firstPart = AudioSegment.from_file(path)
-secondPart = AudioSegment.from_file(path)
-thirdPart = AudioSegment.from_file(path)
-
-firstPart = firstPart[st1:end1]
-secondPart = secondPart[st2:end2]
-thirdPart = thirdPart[st3:end3]
+    st = st * 1000  # Works in milliseconds
+    end = end * 1000
+    clip = AudioSegment.from_file(path)
+    clip = clip[st:end]
+    partList.append(clip)
 
 path = filedialog.askdirectory()
-print(path)
 
+for j in range(len(partList)):
+    partList[j].export(path + "/" + songName + f"{j}Part.wav", format="wav")
 
-firstPart.export(path + "/" + songName + "1Part.wav", format="wav")
-secondPart.export(path + "/" + songName + "2Part.wav", format="wav")
-thirdPart.export(path + "/" + songName + "3Part.wav", format="wav")
+print("DONE :D")
