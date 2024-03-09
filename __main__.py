3fc12972c5ae217011fa4f535830df6947448c1f
ZMiK0 <kbelz21z@gmail.com>
2024-03-09 19:15:57 +0100

    New class Song for better code

--- __main__.py
+++ __main__.py
@@ -1,59 +1,23 @@
 from pydub import AudioSegment
 import tkinter
 from tkinter import filedialog
+import song
 
 print("Welcome to 3 Partes")
 tkinter.Tk().withdraw()
 path = filedialog.askopenfilename()
 
-forma = int(input("What type of file do you want to use: \n1. mp3 \n2. wav\nAnswer: "))
+actualSong = song.Song()
 
-goodChoice = False
-while not goodChoice:
-    if forma == 1:
-        songName = path.replace(".mp3", "")
-        goodChoice = True
-    elif forma == 2:
-        songName = path.replace(".wav", "")
-        goodChoice = True
-    else:
-        print("Invalid file type")
+actualSong.setFormat(path)
+actualSong.setTitle()
 
-songName = songName[::-1]
-wrongCharacterFound = False
-i = 0
-while (not wrongCharacterFound and i < len(songName)):
-    if songName[i] == "/":
-        songName = songName.split("/", 1)
-        songName = songName[0]
-        wrongCharacterFound = True
-    elif songName[i] == "\\":
-        songName = songName.split("\\", 1)
-        songName = songName[0]
-        wrongCharacterFound = True
-    else:
-        i += 1
-
-songName = songName[::-1]
-print("Selected song: " + songName)
 partAmmount = int(input("Introduce how many parts do you want: "))
 
-partList = []
-
-for i in range(partAmmount):
-    print(f"Now choose the {i + 1}º interval in seconds:")
-    st = int(input("Start: "))
-    end = int(input("End: "))
-
-    st = st * 1000  # Works in milliseconds
-    end = end * 1000
-    clip = AudioSegment.from_file(path)
-    clip = clip[st:end]
-    partList.append(clip)
+actualSong.addPart(partAmmount,path)
 
 path = filedialog.askdirectory()
 
-for j in range(len(partList)):
-    partList[j].export(path + "/" + songName + f"{j+1}Part.wav", format="wav")
+actualSong.exportParts(path)
 
 print("DONE :D")
---
+++ song.py
@@ -0,0 +1,60 @@
+import this
+
+from pydub import AudioSegment
+
+class Song(object):
+    title = ""
+    partList = []
+    def __init__(self,):
+        this.title = ""
+
+    def setFormat(self, path):
+        goodChoice = False
+        while not goodChoice:
+            forma = int(input("What type of file do you want to use: \n1. mp3 \n2. wav\nAnswer: "))
+            if forma == 1:
+                self.title = path.replace(".mp3", "")
+                goodChoice = True
+            elif forma == 2:
+                self.title = path.replace(".wav", "")
+                goodChoice = True
+            else:
+                print("Invalid file type")
+
+    def setTitle(self):
+        self.title = self.title[::-1]
+        wrongCharacterFound = False
+        i = 0
+        while (not wrongCharacterFound and i < len(self.title)):
+            if self.title[i] == "/":
+                self.title = self.title.split("/", 1)
+                self.title = self.title[0]
+                wrongCharacterFound = True
+            elif self.title[i] == "\\":
+                self.title = self.title.split("\\", 1)
+                self.title = self.title[0]
+                wrongCharacterFound = True
+            else:
+                i += 1
+
+        self.title = self.title[::-1]
+        return ("Selected song: " + self.title)
+
+    def getName(self):
+        return self.title
+
+    def addPart(self,partAmmount,path):
+        for i in range(partAmmount):
+            print(f"Now choose the {i + 1}º interval in seconds:")
+            st = int(input("Start: "))
+            end = int(input("End: "))
+
+            st = st * 1000  # Works in milliseconds
+            end = end * 1000
+            clip = AudioSegment.from_file(path)
+            clip = clip[st:end]
+            self.partList.append(clip)
+
+    def exportParts(self, path):
+        for j in range(len(self.partList)):
+            self.partList[j].export(path + "/" + self.title + f"{j + 1}Part.wav", format="wav")
